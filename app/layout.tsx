2a545ceba27494a01d0cab4d4cd516a8f39794a4
manray159 <hshs28868@gmail.com>
2026-03-07 04:00:59 -0800

    init: hashtag app

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="ru">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#17212b" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Hashtag" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
